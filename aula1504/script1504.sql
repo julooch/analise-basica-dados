5967c4a4c88aaaa3fb0ed55598b2c5121202bf23
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-04-15 22:29:27 -0300

    Aula 1504 - Calculos de datas e subquerys

---
+++ aula1504/script1504.sql
@@ -0,0 +1,16 @@
+use empregados_db;
+select * from empregado where cod_depto in(
+	select cod_depto from departamento where cod_depto % 2 = 0
+);
+
+/*Cálculo com Datas*/
+
+select *, floor((datediff(now(), dt_nascimento) / 365)) as idade from dependente;
+
+select * from empregado where cod_emp in(
+	select cod_emp from dependente where floor((datediff(now(), dt_nascimento) / 365)) < 18
+);
+
+
+
+
